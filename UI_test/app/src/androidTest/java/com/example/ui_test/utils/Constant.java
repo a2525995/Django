fba8b5f6884612e3072f3fd90940cb4f675621fb
root <root@cnbjlx21783.corpusers.net>
2018-04-12 15:21:28 +0800

    uiautomator

---
+++ UI_test/app/src/androidTest/java/com/example/ui_test/utils/Constant.java
@@ -0,0 +1,59 @@
+package com.example.ui_test.utils;
+
+public class Constant{
+
+
+//The package-name of Sony mobile main menu
+    public static final String PACKAGE_HOME = "com.sonymobile.home";
+    public static final String PACKAGE_ASSIST = "com.sonymobile.assist";
+//There are  APPs'package-name in Sony mobile xz1
+    public static final String PACKAGE_PHONE = "com.sonymobile.android.dialer";
+    public static final String PACKAGE_MAIL = "jp.softbank.mb.mail";
+    public static final String PACKAGE_CHROME = "com.android.chrome";
+    public static final String PACKAGE_CAMERA = "com.sonyericsson.android.camera";
+    public static final String PACKAGE_MUSIC = "com.sonyericsson.music";
+    public static final String PACKAGE_ALBUM = "com.sonyericsson.album";
+    public static final String PACKAGE_PLAY_STORE = "com.google.android.gms";
+    public static final String PACKAGE_CONTACTS = "com.sonymobile.android.contacts";
+    public static final String PACKAGE_NEWS = "com.sony.nfx.app.sfrc";
+    public static final String PACKAGE_WEATHER = "com.sonymobile.xperiaweather";
+    public static final String PACKAGE_TV = "jp.co.fsi.fs1seg";
+    public static final String PACKAGE_CALENDAR = "com.google.android.calendar";
+    public static final String PACKAGE_CALCULATOR = "com.sonymobile.exactcalculator";
+    public static final String PACKAGE_CLOCK = "com.sonyericsson.organizer";
+    public static final String PACKAGE_SETTING = "com.android.settings";
+    public static final String PACKAGE_3DCREATOR = "com.sonymobile.scan3d";
+    public static final String PACKAGE_GOOGLE = "com.google.android.googlequicksearchbox";
+    public static final String PACKAGE_GMAIL = "com.google.android.gm";
+    public static final String PACKAGE_MAPS = "com.google.android.apps.maps";
+    public static final String PACKAGE_YOUTUBE = "com.google.android.youtube";
+    public static final String PACKAGE_DRIVE = "com.google.android.apps.docs";
+    public static final String PACKAGE_PLAY_MUSIC = "com.google.android.music";
+    public static final String PACKAGE_MOVIES_TV = "com.google.android.videos";
+    public static final String PACKAGE_DUO = "com.google.android.apps.tachyon";
+    public static final String PACKAGE_PHOTOS = "com.google.android.apps.photos";
+    public static final String PACKAGE_MESSAGES = "com.google.android.apps.messaging";
+    public static final String PACKAGE_DOCS = "com.google.android.apps.docs.editors.docs";
+    public static final String PACKAGE_SHEETS = "com.google.android.apps.docs.editors.sheets";
+    public static final String PACKAGE_SLIDES = "com.google.android.apps.docs.editors.slides";
+    public static final String PACKAGE_SKETCH = "com.sonymobile.sketch";
+    public static final String PACKAGE_MOVIECREATOR = "com.sonymobile.moviecreator.rmm";
+    public static final String PACKAGE_EMAIL = "com.sonymobile.email";
+    public static final String PACKAGE_FILES = "com.android.documentsui";
+    public static final String PACKAGE_WIFE_SUPPORT_SETTING = "jp.co.softbank.wispr.froyo";
+    public static final String PACKAGE_SPOTIFY = "com.spotify.music";
+    public static final String PACKAGE_MORA = "jp.co.labelgate.moratouch";
+    public static final String PACKAGE_WHAT_IS_NEW = "com.sonymobile.entrance";
+    public static final String PACKAGE_AMAZON = "com.amazon.mShop.android.shopping";
+    public static final String PACKAGE_APP_PASS = "com.mobiroo.xgen";
+    public static final String PACKAGE_MY_SOFTBANK_PLUS = "jp.softbank.mb.dtm";
+    public static final String PACKAGE_SMARTPHONE_BEGIN_GUIDE = "jp.softbank.beginnerguide";
+    public static final String PACKAGE_XPERIA_LOUNGE = "com.sonyericsson.androidapp.sehome";
+    public static final String PACKAGE_SPOT_LIST = "jp.sony.spot.sp";
+    public static final String PACKAGE_OSAIFU_KEITAI = "com.felicanetworks.mfm.main";
+    public static final String PACKAGE_FACEBOOK ="com.facebook.appmanager";
+    public static final String PACKAGE_SIMPLE_BIZ = "jp.softbank.mb.bhssimple";
+    public static final String PACKAGE_CRASHLOGUPLOADER = "com.sonymobile.crashloguploader";
+
+
+}
